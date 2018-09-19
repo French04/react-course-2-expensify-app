ec9e748a38a02df77ae1605d0932d3a22871d22c
Andrea Franciosi <andreafranciosi@MBP-di-Andrea.homenet.telecomitalia.it>
2018-09-19 11:12:29 +0200

    fetching database from firebase

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -65,3 +65,12 @@ test('not edit an expense if id not found test', () => {
     const state = expensesReducer(expenses, action);
     expect(state).toEqual(expenses);
 });
+
+test('set expenses test', () => {
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[1]]
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[1]]);
+});
